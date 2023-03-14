b44512b49cfd2691c0f91dae11884e34fa14718b
Kenedy228 <112537849+Kenedy228@users.noreply.github.com>
2023-03-15 08:01:58 +1000

    11

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 let tg = window.Telegram.WebApp;
-tg.MainButton.show();
+let form = document.querySelector(".appointment");
+
+const appointment = {
+    name,
+    doctor,
+    phone,
+    comment
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    appointment.name = document.querySelector(".name").value;
+    appointment.doctor = document.querySelector(".doctor").value;
+    appointment.phone = document.querySelector(".phone").value;
+    appointment.comment = document.querySelector(".comment").value;
+
+    tg.MainButton.show();
+})
 
 tg.onEvent('mainButtonClicked', () => {
-    let name = document.querySelector(".name").value;
-    let doctor = document.querySelector(".doctor").value;
-    let phone = document.querySelector(".phone").value;
-    let comment = document.querySelector(".comment").value;
 
-    const appointment = {
-        name,
-        doctor,
-        phone,
-        comment
-    }
-
-    for (let value of Object.values(appointment)) {
-        alert(value);
-    }
+    fetch('http://localhost:8000', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(appointment)
+    });
 })
