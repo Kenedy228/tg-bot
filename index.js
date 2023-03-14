9535644670f5cbc2555681da556df907a5b31609
Kenedy228 <112537849+Kenedy228@users.noreply.github.com>
2023-03-14 14:04:45 +1000

    11

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 let tg = window.Telegram.WebApp;
 tg.MainButton.show();
-let counter = document.querySelector(".counter");
-let count = 0;
 
 tg.onEvent('mainButtonClicked', () => {
     let name = document.querySelector(".name").value;
