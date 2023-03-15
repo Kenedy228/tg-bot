20bc9aadb56a89b1317930da81491a0d75e3685b
Kenedy228 <112537849+Kenedy228@users.noreply.github.com>
2023-03-16 09:37:33 +1000

    update

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 let tg = window.Telegram.WebApp;
-let queryId = tg.initDataUnsafe.query_id;
 tg.MainButton.show();
-const axios = require("axios");
 
 tg.onEvent('mainButtonClicked', () => {
 
-    const url = 'http://185.143.174.146:8000/web-data'
-    const data = {
+    tg.sendData(JSON.stringify({
         name: "Данил",
         phone: "+79144284899",
-        queryId
-    }
-
-    axios.post(url, data, {
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json;charset=UTF-8",
-        },
-    })
+        comment: "Болит зуб"
+    }))
 
 })
