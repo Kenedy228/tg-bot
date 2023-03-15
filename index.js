2848fda7acf48a88a951b0866fc56d4f05a9c845
Kenedy228 <112537849+Kenedy228@users.noreply.github.com>
2023-03-15 16:39:40 +1000

    update

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 let tg = window.Telegram.WebApp;
 let form = document.querySelector("#appointment");
-
+let queryId = tg.initDataUnsafe?.query_id;
+let name = "";
+let phone = "";
+let comment = "";
 const appointment = {}
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    appointment.name = document.querySelector(".name").value;
-    appointment.phone = document.querySelector(".phone").value;
-    appointment.comment = document.querySelector(".comment").value;
-    appointment.queryID = tg.initDataUnsafe.query_id;
+    name = document.querySelector(".name").value;
+    phone = document.querySelector(".phone").value;
+    comment = document.querySelector(".comment").value;
 
     tg.MainButton.show();
 });
 
 tg.onEvent('mainButtonClicked', () => {
+
+    const data = {
+        name,
+        phone,
+        comment,
+        queryId
+    }
+
     fetch('http://192.168.0.2:8000/web-data', {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(appointment)
+        body: JSON.stringify(data)
     });
 
 })
