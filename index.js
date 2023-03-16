f84a5243ad5961fa56540f83a478ed2723515433
Kenedy228 <112537849+Kenedy228@users.noreply.github.com>
2023-03-16 10:13:42 +1000

    update

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 let tg = window.Telegram.WebApp;
 tg.MainButton.show();
 
+const form = document.querySelector(".appointment");
+
 tg.onEvent('mainButtonClicked', () => {
 
-    tg.sendData(JSON.stringify({
-        name: "Данил",
-        phone: "+79144284899",
-        comment: "Болит зуб"
-    }))
+    const name = document.querySelector(".name").value;
+    const phone = document.querySelector(".phone").value;
+    const comment = document.querySelector(".comment").value;
+
+    if (name && phone) {
+        tg.sendData(JSON.stringify({
+            name,
+            phone,
+            comment
+        }))
+    } else {
+        alert("Заполните все поля со *");
+    }
 
 })
