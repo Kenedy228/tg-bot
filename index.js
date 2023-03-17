005e90524513d716226c7ab24c74b2b79a764f4f
Kenedy228 <112537849+Kenedy228@users.noreply.github.com>
2023-03-18 09:42:55 +1000

    update

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ tg.MainButton.text = "Оставить заявку";
 tg.MainButton.color = "#00b1a8";
 tg.MainButton.show();
 
+const phone = document.querySelector(".phone");
+
+const maskOptions = {
+    mask: '+{7}(000)000-00-00'
+};
+const mask = IMask(phone, maskOptions);
+
 tg.onEvent('mainButtonClicked', () => {
 
     const name = document.querySelector(".name").value;
