8c5d635afd7f548511784af53f6d81e71e3fb826
Kenedy228 <112537849+Kenedy228@users.noreply.github.com>
2023-03-19 08:06:54 +1000

    update

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const express = require("express");
+let tg = window.Telegram.WebApp;
+tg.MainButton.text = "Оставить заявку";
+tg.MainButton.color = "#00b1a8";
+tg.MainButton.show();
 
-const app = express();
-const port = 8000;
+const phone = document.querySelector(".phone");
 
-app.listen(8000, () => console.log("server started at port: " + port));
+const maskOptions = {
+    mask: '+{7}(000)000-00-00'
+};
+const mask = IMask(phone, maskOptions);
+
+tg.onEvent('mainButtonClicked', () => {
+
+    const name = document.querySelector(".name").value;
+    const phone = document.querySelector(".phone").value;
+    const comment = document.querySelector(".comment").value;
+
+    if (name && phone) {
+        tg.sendData(JSON.stringify({
+            name,
+            phone,
+            comment
+        }))
+    } else {
+        tg.showAlert("Заполните все поля со *");
+    }
+
+})
